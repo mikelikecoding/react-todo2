d23df86f78e64db42b6afa0c09bb182b032d1133
Michael Calvin Holmes <michaelcalvinholmes@Michaels-MacBook-Air.local>
2023-09-30 11:07:49 -0700

    Made changes to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import * as React from 'react';
 
+function getTitle(title) {
+  return title;
+}
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-</div> );
-}
+<div>
+      <h1>Hello {getTitle('React')}</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+); }
 
 export default App;

bde7591a4a4d3855aea01dd4ed787156217c1235
Michael Calvin Holmes <michaelcalvinholmes@Michaels-MacBook-Air.local>
2023-09-29 21:33:24 -0700

    Second component commet

--- src/App.js
+++ src/App.js
@@ -1,20 +1,5 @@
 import React from 'react';
 
-// function App() {
-//   return (
-//     <div style={{ textAlign: 'center' }}>
-//       <header>
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 function App() {
   return (
     <div>

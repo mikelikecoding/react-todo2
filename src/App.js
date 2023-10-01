2a1687cd29dc09b335862263e6cebe7bc2fe09f5
Michael Calvin Holmes <michaelcalvinholmes@Michaels-MacBook-Air.local>
2023-09-30 22:41:56 -0700

    The finish todo list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 import * as React from 'react';
 
-function getTitle(title) {
-  return title;
-}
+{/*This is an array of object to create a list*/}
+
+const todoList = [
+  {
+    title: 'HelloReact',
+    objectID: 1,
+}, {
+    title: 'Requirements',
+    objectID: 2,
+},{
+  title: 'SettingupaReactProject',
+  objectID: 3,
+},{
+  title: 'MeettheReactComponent.',
+  objectID: 4,
+},{
+  title: 'React JSX',
+  objectID: 5,
+},{
+  title: 'Lists in React',
+  objectID: 6,
+},
+
+
+];
+
+{/*This is the app that will display the arrays of Object*/}
+
 function App() {
   return (
 <div>
-      <h1>Hello {getTitle('React')}</h1>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-    </div>
-); }
+      <h1>Todo List</h1>
+
+<hr />
+    <ul>
+        {/*This map function will iterate through the array of object*/}
+
+      {todoList.map((item) =>
+        <li key={item.objectID} >Chapter  {item.objectID}:  {item.title}
+        </li> )}
+
+    </ul>
+</div> );
+}
 
 export default App;

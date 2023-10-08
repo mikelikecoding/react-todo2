af74b7b912730eed2d0731f2e2d4270f1ef493fa
Michael Calvin Holmes <michaelcalvinholmes@Michaels-MacBook-Air.local>
2023-10-07 21:22:56 -0700

    The last commit/ week 2

---
+++ src/AddTodoForm.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+
+function AddTodoForm () {
+   return (
+    <form>
+      <label htmlFor="todoTitle">Title: </label>
+      <input id="todoTitle" type="text" />
+      <button>Add</button>
+    </form>
+
+   )
+}
+
+
+
+export default AddTodoForm;

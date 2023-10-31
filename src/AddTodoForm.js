f5912676f867318945e0ff6c2496bb5d9e0fbf95
Michael Calvin Holmes <michaelcalvinholmes@Michaels-MacBook-Air.local>
2023-10-30 20:38:51 -0700

    commit for pull request lesson_1_3

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -6,8 +6,8 @@ function AddTodoForm (props) {
  const handleAddTodo = (event) => {
         event.preventDefault();
      const todoTitle = event.target.title.value;
-        console.log(todoTitle, `ARE YOU HERE111`);
-      const form = document.querySelector('form');
+     console.log(todoTitle, `ARE YOU HERE111`);
+     const form = document.querySelector('form');
       form.reset();
       props.onAddTodo(todoTitle);
 

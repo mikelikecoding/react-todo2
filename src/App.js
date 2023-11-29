2f9eae41f802817c055c7d8e49e16b4422b073bc
Michael Calvin Holmes <michaelcalvinholmes@Michaels-Air.lan>
2023-11-28 16:20:09 -0800

    For pull request 1.5

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
-import React from 'react';
-import TodoList from './TodoList';
-import AddTodoForm from './AddTodoForm'
-// import Search from './Search';
-import {useState} from 'react';
+import React from "react";
+import TodoList from "./TodoList";
+import AddTodoForm from "./AddTodoForm";
+import { useState, useEffect } from "react";
 
 // /*This is the app that will display the arrays of Object and the form with button*
 
+const useSemiPersistentState = () => {
+  const [todoList, setTodoList] = useState(
+    JSON.parse(localStorage.getItem("savedTodoList")) || []
+  );
+
+  useEffect(() => {
+    // Save todoList to localStorage whenever it changes
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+  }, [todoList]); // `todoList` specified as a dependency
+
+  return [todoList, setTodoList];
+};
+
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  // const [todoList, setTodoList] = [];
-const addTodo = (newTodo) => {
-  setTodoList([...todoList, newTodo])
-}
+  const [todoList, setTodoList] = useSemiPersistentState();
+
+  const addTodo = (newTodo) => {
+    setTodoList([...todoList, newTodo]);
+  };
 
   return (
-    <div>
+    <>
       <h1>Todo List</h1>
 
-   {/* <Search /> */}
-  <AddTodoForm  onAddTodo={addTodo}/>
+      <AddTodoForm onAddTodo={addTodo} />
 
-<hr />
- 
- <TodoList todoList={todoList} />  
-</div> );
+      <hr />
 
+      <TodoList todoList={todoList} />
+    </>
+  );
 }
-
 
 export default App;

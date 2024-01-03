19711231a24c2500be219ddec2bad0862b7c8de1
Michael Calvin Holmes <michaelcalvinholmes@Michaels-Air.lan>
2024-01-02 20:25:10 -0800

    Revised

--- src/App.js
+++ src/App.js
@@ -3,48 +3,58 @@ import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 import { useState, useEffect } from "react";
 
+// /*This is the app that will display the arrays of Object and the form with button*
 
 
 function App() {
-  const [todoList, setTodoList] = useState(
-    JSON.parse(localStorage.getItem("savedTodoList")) || []
-  );
 
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({ data: { todoList: todoList } });
-      }, 2000);
-    }).then((result) => {
-      setTodoList(result.data.todoList);
-      setIsLoading(false);
-    });
-  });
-
-  useEffect(() => {
-    if (!isLoading) {
-      localStorage.setItem("todoList", JSON.stringify(todoList));
-    }
-  }, [todoList, isLoading]);
-
-  const removeTodo = (idToRemove) => {
-    const updatedTodoList = todoList.filter((item) => item.id !== idToRemove);
-    setTodoList(updatedTodoList);
-  };
-
-  const addTodo = (newTodo) => {
-    setTodoList([...todoList, newTodo]);
-  };
-
-  return (
-    <>
+  
+ 
+  const [todoList, setTodoList] = useState([]);
+    
+    const [isLoading, setIsLoading] = useState(true);
+    
+    
+    useEffect(() => {
+      new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve({ data: { todoList: JSON.parse(localStorage.getItem('savedTodoList')) || [], } });
+        },2000)
+      })
+      .then((result) => {
+        setTodoList(result.data.todoList);
+        setIsLoading(false);
+      });
+    },[]);
+    
+    
+    useEffect(() => {
+      if (!isLoading) {
+        localStorage.setItem('todoList', JSON.stringify(todoList));
+      }
+    }, [todoList, isLoading]);
+    
+    
+    const removeTodo = (idToRemove) => {
+      const updatedTodoList = todoList.filter((item) => item.id !== idToRemove);
+      setTodoList(updatedTodoList);
+    };
+    
+    const addTodo = (newTodo) => {
+      setTodoList([...todoList, newTodo]);
+    };
+    
+    return (
+      <>
       <h1>Todo List</h1>
-      <AddTodoForm onAddTodo={addTodo} />
+        <AddTodoForm onAddTodo={addTodo} />
       <hr />
-      {isLoading ? <p>Page Is Loading....</p> : TodoList}
-      <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+      {isLoading ? (
+        <p>Page Is Loading....</p>
+      ) : (
+        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+      )}
+       <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
     </>
   );
 }
